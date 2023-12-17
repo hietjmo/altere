a429d839b9b1464e5943a6eb856e277c3835cea7
Jarmo Hietala <68544023+hietjmo@users.noreply.github.com>
2023-12-17 22:27:00 +0200

    Add files via upload

---
+++ clipboard/clipboard.py
@@ -0,0 +1,24 @@
+
+import tkinter
+
+win = tkinter.Tk ()
+selection = "CLIPBOARD"
+# selection = "PRIMARY"
+
+class S ():
+  def __init__ (self):
+    self.clipboard = win.selection_get (selection=selection)
+    self.old_clipboard = self.clipboard 
+  def check_clipboard (self):
+    self.clipboard = win.selection_get (selection=selection)
+    if self.clipboard != self.old_clipboard:
+      print (self.clipboard)
+      self.old_clipboard = self.clipboard
+    win.after (100, self.check_clipboard)
+
+self = S ()
+win.title (selection)
+win.after (100, self.check_clipboard)
+win.mainloop ()
+
+
